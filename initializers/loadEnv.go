e1612c286c3d9c935548ea7b55a07ff8024d15a0
Kariqs <kariukibenard189@gmail.com>
2025-04-21 22:13:33 +0300

    Added some changes.

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,13 +2,18 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		} else {
+			log.Println(".env file loaded")
+		}
 	}
 }

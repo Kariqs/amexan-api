afb07c7235ef774b90aa581050582665f667fa75
Kariqs <kariukibenard189@gmail.com>
2025-04-28 08:56:54 +0300

    Added functionality to fetch products.

--- routes/productRoutes.go
+++ routes/productRoutes.go
@@ -9,4 +9,6 @@ func ProductRoutes(server *gin.Engine) {
 	server.POST("/product", controllers.CreateProduct)
 	server.POST("/product-specs", controllers.CreateProductSpecs)
 	server.POST("/product-images", controllers.UploadProductImages)
+	server.GET("/product", controllers.GetProducts)
+	server.GET("/product/:id", controllers.GetProduct)
 }

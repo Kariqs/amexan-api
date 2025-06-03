4bc19f16c46bc9cd95aba04d83937a5acd3d109e
Kariqs <kariukibenard189@gmail.com>
2025-06-04 00:49:32 +0300

    Added an explaination.

--- controllers/defaultController.go
+++ controllers/defaultController.go
@@ -7,6 +7,33 @@ import (
 )
 
 func GetHome(ctx *gin.Context) {
-	welcomeMessage := "Welcome to Amexan API ❤️. Enjoy seamless interection with this API ❤️."
-	ctx.JSON(http.StatusOK, welcomeMessage)
+	message := `Welcome to Amexan API ❤️. Enjoy seamless interaction with this API.
+
+The following are the endpoints for this API:
+
+AUTH
+- POST "/signup" - Create user account
+- POST "/login" - Access user account
+- POST "/verify-email/:activationToken" - Activate user account
+- POST "/forgot-password" - Request password reset
+- POST "/reset-password/:resetToken" - Reset user password
+
+PRODUCT
+- POST "/product" - Create new product
+- GET "/product" - Get all products
+- POST "/product-specs" - Add product specifications
+- POST "/product-images" - Add product images
+- GET "/product/{id}" - Get product by ID
+
+ORDER
+- POST "/order" - Create a new order
+- GET "/order" - Retrieve all orders
+- GET "/user/:userId/orders" - Get orders for a specific user
+- GET "/order/:orderId" - Get order by ID
+- PATCH "/order/:orderId" - Update order status
+- DELETE "/order/:orderId" - Delete order by ID`
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": message,
+	})
 }
